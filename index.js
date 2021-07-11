5f923a0bc4ba6dce2ad1a269b9f62c1f1f6eafc8
Kayla Phelps <kmphelps822@gmail.com>
2021-07-10 22:49:10 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('H1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'Kayla is the champion';
+
